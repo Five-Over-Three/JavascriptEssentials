d8a4c2b5d9e3b077768df4838250bbae20d1480b
FiveOverThree <fiveoverthreetv@gmail.com>
2025-02-05 11:27:08 -0500

    Added scope lab files

---
+++ Scope/scope_lab.js
@@ -0,0 +1,18 @@
+//Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+    //block scope
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped constant";
+}
+
+//global scope
+console.log(globalVar);
+console.log(globalLet);
+console.log(globalConst);
+)
